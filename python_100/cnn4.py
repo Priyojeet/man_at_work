e6a6e3a326882909de5148396efb28cc7d31f8c1
Priyojeet <37825783+Priyojeet@users.noreply.github.com>
2019-06-15 13:56:38 +0530

    Update cnn4.py

--- python_100/cnn4.py
+++ python_100/cnn4.py
@@ -7,10 +7,29 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Dropout
 
 
+columns = ["cat's", "dog's", "jeet's", "subhajit's", "sudhanshu's"]
+#print(columns)
+
+datagen = ImageDataGenerator(
+	rescale=1./255,
+	shear_range=0.2,
+	zoom_range=0.2,
+	horizontal_flip=True)
+
+test_datagen=ImageDataGenerator(rescale=1./255)
+
+train_generator = datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (100, 100),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
+valid_generator = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (100, 100),
+                                            batch_size = 32,
+                                            class_mode = 'categorical')
 
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), input_shape = (200, 200, 3), activation = 'relu'))
+model.add(Conv2D(32, (3, 3), input_shape = (100, 100, 3), activation = 'relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(32, (3, 3), activation='relu'))
@@ -22,48 +41,29 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(64, activation = 'relu'))
 model.add(Dropout(0.5))
-model.add(Dense(5, activation = 'softmax'))
+model.add(Dense(5, activation = 'sigmoid'))
 
 model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop',
+              optimizer='adam',
               metrics=['accuracy'])
-
-
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
-
-test_datagen = ImageDataGenerator(rescale=1./255)
-
-train_generator = train_datagen.flow_from_directory('dataset/training_set',
-                                                 target_size = (200, 200),
-                                                 batch_size = 32,
-                                                 class_mode = 'categorical')
-
-validation_generator = test_datagen.flow_from_directory('dataset/test_set',
-                                            target_size = (200, 200),
-                                            batch_size = 32,
-                                            class_mode = 'categorical')
-#print("validation_generators", validation_generator)
 
 model.fit_generator(
     train_generator,
-    samples_per_epoch=8000,
-    epochs=1,
-    validation_data=validation_generator,
-    validation_steps=2000)
+    steps_per_epoch=250,
+    epochs=2,
+    validation_data=valid_generator,
+    validation_steps=100)
+
 
 
 model.save('image_rec_test.h5')
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('/home/lucifer/Downloads/61833371_144640713262993_8942835081001566208_o.jpg', target_size = (200, 200, 3))
+test_image = image.load_img('/home/lucifer/Downloads/61833371_144640713262993_8942835081001566208_o.jpg', target_size = (100, 100, 3))
 test_image = image.img_to_array(test_image)
 test_image = test_image/225
 #test_image = np.expand_dims(test_image, axis = 0)
-result = model.predict(test_image.reshape(1, 200, 200, 3))
+result = model.predict(test_image.reshape(1, 100, 100, 3))
 print(result)
 pred_bool = (result >0.5)
 print(pred_bool)
