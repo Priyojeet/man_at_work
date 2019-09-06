efa431874fa55c62fcc91ca9f0e9c267ec1cb5a5
Priyojeet Bhunia <37825783+Priyojeet@users.noreply.github.com>
2019-09-06 12:38:18 +0530

    Delete tsk2ex4.py

--- tsk2ex4.py
+++
@@ -1,1 +0,0 @@
-print("WE, THE PEOPLE OF INDIA, \n\thaving solemnly resolved to constitute India into a SOVEREIGN, \n\t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC \n\t\tand to secure to all its citizens")
